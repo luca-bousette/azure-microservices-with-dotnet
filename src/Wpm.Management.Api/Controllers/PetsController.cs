f591d0081d6f4c2071ec67fb9f7c7effe33fef9e
Luca Bousette <6220689@johnabbottcollege.net>
2025-12-10 21:27:22 -0500

    initital commit

---
+++ src/Wpm.Management.Api/Controllers/PetsController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Wpm.Management.Api.DataAccess;
+
+namespace Wpm.Management.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PetsController(ManagementDbContext dbContext) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult> Get()
+    {
+        var all = await dbContext.Pets.Include(p => p.Breed).ToListAsync();
+        return Ok(all);
+    }
+
+    [HttpGet("{id}", Name = nameof(GetById))]
+    public async Task<ActionResult> GetById(int id)
+    {
+        var pet = await dbContext.Pets.Include(p => p.Breed).Where(p => p.Id == id).FirstOrDefaultAsync();
+        return Ok(pet);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(NewPet newPet)
+    {
+        var pet = newPet.ToPet();
+        await dbContext.Pets.AddAsync(pet);
+        await dbContext.SaveChangesAsync();
+
+        return CreatedAtRoute(nameof(GetById), new { id = pet.Id }, newPet);
+    }
+}
+
+public record NewPet(string Name, int Age, int BreedId)
+{
+    public Pet ToPet()
+    {
+        return new Pet() { Name = Name, Age = Age, BreedId = BreedId };
+    }
+}
---
+++ src/Wpm.Management.Api/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using Wpm.Management.Api.DataAccess;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ManagementDbContext>(options =>
+{
+    options.UseInMemoryDatabase("WpmManagement");
+});
+
+var app = builder.Build();
+
+app.EnsureDbIsCreated();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
