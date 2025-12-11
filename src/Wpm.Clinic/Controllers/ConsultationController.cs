16a5fa7f94e8405ff0db6d027544245ab1aaa251
Luca Bousette <6220689@johnabbottcollege.net>
2025-12-11 00:23:50 -0500

    changed yml and fixed clinic stuff

--- src/Wpm.Clinic/Controllers/ConsultationController.cs
+++ src/Wpm.Clinic/Controllers/ConsultationController.cs
@@ -7,7 +7,7 @@ namespace Wpm.Clinic.Controllers
     [Route("[controller]")]
     public class ConsultationController(ClinicApplicationService applicationService) : ControllerBase
     {
-        [HttpPost("/start")]
+        [HttpPost("start")]
         public async Task<IActionResult> Start(StartConsultationCommand command)
         {
             var result = await applicationService.Handle(command);
